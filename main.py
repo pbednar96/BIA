732ad0c3b988bf249c743775dfdb2d0f0e9463cb
petr <petan.bednar@gmail.com>
2019-10-07 21:48:46 +0200

    hm, good improvement

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import func_file, static_data, show_3D_graph, blind_search
 
 FUNCTION = "sphere"
+NUM_GENERETED_POINTS = 20
 
 
 def main():
-    x = blind_search.blind_search(FUNCTION, 20)
+    x = blind_search.blind_search(FUNCTION, NUM_GENERETED_POINTS)
     show_3D_graph.show_both_graph(FUNCTION, x)
 
 if __name__ == "__main__":
