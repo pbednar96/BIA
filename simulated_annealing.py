f3cce997cdb6d90a2f32afe1b5d70cd6ffd40a6e
petr <petan.bednar@gmail.com>
2019-10-15 09:22:49 +0200

    improve simulated_annealing

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -5,7 +5,7 @@ import random
 
 
 def simulated_annealing(name_function, mux, muy, sigma, size, iterat, temperature_0):
-    temperature_decrease = lambda temperature: temperature * 0.99
+    temperature_decrease = lambda temperature: temperature * 0.95
     temperature_min = 0.1
     list_points = []
     generated_x = np.random.normal(mux, sigma, size)
@@ -13,7 +13,8 @@ def simulated_annealing(name_function, mux, muy, sigma, size, iterat, temperatur
     tmp = func_file.return_value_function([mux, muy], name_function)
     cord_X = generated_x[0]
     cord_Y = generated_y[0]
-    if temperature_0 > temperature_min:
+
+    while temperature_0 > temperature_min:
         for i, j in zip(generated_x, generated_y):
             min = func_file.return_value_function([i, j], name_function)
             list_points.append([i, j, min])
@@ -27,16 +28,23 @@ def simulated_annealing(name_function, mux, muy, sigma, size, iterat, temperatur
                     tmp = min
                     cord_X = i
                     cord_Y = j
+            print(temperature_0)
+            # print("Iteration n.: " + str(iterat))
+            print("X: " + str(cord_X))
+            print("Y: " + str(cord_Y))
+            print("VALUE: " + str(tmp))
+            print("")
             temperature_0 = temperature_decrease(temperature_0)
 
-    print("Iteration n.: " + str(iterat))
+    print(temperature_0)
+    #print("Iteration n.: " + str(iterat))
     print("X: " + str(cord_X))
     print("Y: " + str(cord_Y))
     print("VALUE: " + str(tmp))
     print("")
 
-
-    if iterat == 1:
-        return [cord_X, cord_Y, tmp]
-    else:
-        return simulated_annealing(name_function, cord_X, cord_Y, sigma, size, iterat - 1, temperature_0)
+    return [cord_X, cord_Y, tmp]
+    # if iterat == 1:
+    #     return [cord_X, cord_Y, tmp]
+    # else:
+    #     return simulated_annealing(name_function, cord_X, cord_Y, sigma, size, iterat - 1, temperature_0)
