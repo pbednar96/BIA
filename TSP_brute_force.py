41d8ccc52e7bcf6dd263b9b74b821d8f8b4872f2
petr <petan.bednar@gmail.com>
2019-10-29 09:23:52 +0100

    TSP - some fix up for TSP

--- TSP_brute_force.py
+++ TSP_brute_force.py
@@ -2,24 +2,22 @@ from datetime import datetime
 
 
 def permutation(list_data):
-    l = []
+    list_perm = []
     if len(list_data) == 1:
         return [list_data]
     for i in range(len(list_data)):
         m = list_data[i]
         new_list = list_data[:i] + list_data[i + 1:]
         for p in permutation(new_list):
-            l.append([m] + p)
-    return l
+            list_perm.append([m] + p)
+    return list_perm
 
 
 def main():
-    data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    data = [1, 2, 3]
     start_time = datetime.now()
-    for p in permutation(data):
-        item = p
+    print(permutation(data))
     end_time = datetime.now()
-
     print(str(end_time - start_time))
 
 
